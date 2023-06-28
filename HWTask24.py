c1ff4a1c89848589f3ea8b69207c8bc7a4e4a614
mad rider <mad-ride.rider@yandex.ru>
2023-06-28 03:21:04 +0300

    task 24 add interface

--- HWTask24.py
+++ HWTask24.py
@@ -13,7 +13,16 @@
 
 
 from random import randint
-list1 = [randint(10, 5000) for i in range(randint(3,100))]
+from tkinter import *
+from tkinter import messagebox
+from random import randint
+
+window = Tk()
+window.title('выбор одинаковых элементов зи двух множеств')
+window.geometry('600x300')
+
+
+list1 = [randint(10, 5000) for i in range(randint(3,20))]
 print(list1)
 numberBush = randint(0,len(list1)-1)
 print(numberBush)
@@ -29,4 +38,48 @@ summHarvest = list1[numberBush] + list1[nextBush] + list1[earlyBush]
 print(summHarvest)
 
 
+frame = Frame(
+    window,
+    padx=10,
+    pady=10
+)
+frame.pack(expand=True)
 
+height_lb = Label(
+    frame,
+    text="урожайность каждого дерева"
+)
+height_lb.grid(row=1, column=2)
+
+height_lb = Label(
+    frame,
+    text = str(list1)
+)
+height_lb.grid(row=2, column=2)
+
+
+height_lb = Label(
+    frame,
+    text="указанное дерево сбора"
+)
+height_lb.grid(row=3, column=2)
+
+height_lb = Label(
+    frame,
+    text = str(numberBush+1)
+)
+height_lb.grid(row=4, column=2)
+
+height_lb = Label(
+    frame,
+    text="сумма общего сбора за один подход"
+)
+height_lb.grid(row=5, column=2)
+
+height_lb = Label(
+    frame,
+    text = str(summHarvest)
+)
+height_lb.grid(row=6, column=2)
+
+window.mainloop()
