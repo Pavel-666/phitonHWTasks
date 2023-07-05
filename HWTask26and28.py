1a493720b030fdfa9c367be9c605b24f804c69f1
mad rider <mad-ride.rider@yandex.ru>
2023-07-06 00:34:33 +0300

    task 26 and 28 with interface

--- HWTask26and28.py
+++ HWTask26and28.py
@@ -4,6 +4,15 @@
 # A = 3; B = 5 -> 243 (3⁵)
 #     A = 2; B = 3 -> 8
 
+# Задача 28: Напишите рекурсивную функцию sum(a, b),
+# возвращающую сумму двух целых неотрицательных чисел.
+# Из всех арифметических операций допускаются только +1 и -1.
+# Также нельзя использовать циклы.
+# *Пример:*
+# 2 2
+#     4
+
+
 def exponentiation(substruct, index):
     if index == 0:
         return 1
@@ -11,3 +20,10 @@ def exponentiation(substruct, index):
         # print(substruct, index)
         return substruct * exponentiation(substruct, index - 1)
 
+
+
+def sum(a, b):
+    if b == 0:
+        return a
+    else:
+        return sum(a + 1, b - 1)
